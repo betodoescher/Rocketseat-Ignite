5467cf3841d02a02e3ec0f86628e7c884f8d59a0
Beto Doescher <betodoescher@gmail.com>
2021-06-08 17:03:37 -0300

    Routes authenticated

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import usersController from '../loaders/users.loaders'
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
 
 const usersRoutes = Router()
+
+usersRoutes.use(ensureAuthenticated)
 
 usersRoutes.route('/users')
     .get((request, response) => {
